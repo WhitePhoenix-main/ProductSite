dfab958715a99b88fc34a29b0907c51a32188ef5
WhitePhoenix-main <72211870+WhitePhoenix-main@users.noreply.github.com>
2021-09-05 22:50:12 +0800

    *Added locale

--- ProductsSite/Startup.cs
+++ ProductsSite/Startup.cs
@@ -39,7 +39,6 @@ namespace ProductsSite
             services.AddDbContext<ProductsSiteContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("ProductsSiteContext")));
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -54,7 +53,8 @@ namespace ProductsSite
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseRequestLocalization("en-UY", "fr-FR", "ru-En");
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
