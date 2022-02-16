d4553ae636140e756da935ca5c85ce682925aa54
WhitePhoenix-main <72211870+WhitePhoenix-main@users.noreply.github.com>
2022-02-16 19:31:24 +0800

    1

---
+++ ProductsSite/Pages/Wishlist/Data/WishlistRecord.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsSite.Pages.Wishlist.Data
+{
+    [Table("Wishlist")]
+    public class WishlistRecord
+    {
+        [Key]
+        [Column(TypeName = "nvarchar(150)")]
+        public string UserId { get; set; }
+
+        [Key]
+        [Column(TypeName = "nvarchar(150)")]
+        public string ProductId { get; set; }
+    }
+}
---
+++ ProductsSite/Pages/Wishlist/Wishlist.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProductsSite.Pages.Wishlist
+{
+    public class WishListCreate : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
